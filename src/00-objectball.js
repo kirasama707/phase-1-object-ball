d6a5acb4c411069042fe38e9704ae30e74c89d6c
christian <vagetto707@aol.com>
2022-04-03 17:15:02 -0700

    completed assignment

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,178 +1,271 @@
-function gameObject() {
+const gameObject = () => {
     return {
-        "home":{
-            "teamName":"Brooklyn Nets",
-            "colors":["Black", "White"],
-            "players":{
-                "Alan Anderson": {
-                "Number": 0,
-                "shoe":16,
-                "points":22,
-                "rebounds":12,
-                "assists":12,
-                "steals":3,
-                "blocks":1,
-                "slamDunks":1,},
-               
-                "Reggie Evans": {
-                "Number":30,
-                "shoe":14,
-                "points":12,
-                "rebounds":12,
-                "assists":12,
-                "steals":12,
-                "blocks":12,
-                "slamDunks":7,},
-                
-                "Brook Lopez": {
-                    "number":11,
-                    "shoe":17,
-                    "points":17,
-                    "rebounds":19,
-                    "assists":10,
-                    "steals":3,
-                    "blocks":1,
-                    "slamDunks":15,
+        home: {
+            teamName: 'Brooklyn Nets',
+            colors: ['black', 'white'],
+            players: {
+                'Alan Anderson': {
+                    number: 0,
+                    shoe: 16,
+                    points: 22,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 1
                 },
-                "Mason Plumlee": {
-                    "number":1,
-                    "shoe":19,
-                    "points":26,
-                    "rebounds":12,
-                    "assists":6,
-                    "steals":3,
-                    "blocks":8,
-                    "slamDunks":5,
+
+                'Reggie Evans': {
+                    number: 30,
+                    shoe: 14,
+                    points: 12,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 12,
+                    blocks: 12,
+                    slamDunks: 7
                 },
-                "Jason Terry": {
-                    "number":31,
-                    "shoe":15,
-                    "points":19,
-                    "rebounds":2,
-                    "assists":2,
-                    "steals":4,
-                    "blocks":11,
-                    "slamDunks":1,
+                'Brook Lopez': {
+                    number: 11,
+                    shoe: 17,
+                    points: 17,
+                    rebounds: 19,
+                    assists: 10,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 15
                 },
-            },
+                'Mason Plumlee': {
+                    number: 1,
+                    shoe: 19,
+                    points: 26,
+                    rebounds: 12,
+                    assists: 6,
+                    steals: 3,
+                    blocks: 8,
+                    slamDunks: 5
+                },
+                'Jason Terry': {
+                    number: 31,
+                    shoe: 15,
+                    points: 19,
+                    rebounds: 2,
+                    assists: 2,
+                    steals: 4,
+                    blocks: 11,
+                    slamDunks: 1
+                }
+
+            }
+
         },
-        "away":{
-            "teamName":"Charlotte Hornets",
-            "colors":["Turquoise", "Purple"],
-            "players":{
-                "Jeff Adrien": {
-                    "Number":4, 
-                    "shoe":18,
-                    "points":10,
-                    "rebounds":1,
-                    "assists":1,
-                    "steals":2,
-                    "blocks":7,
-                    "slamDunks":2,
+
+        away: {
+            teamName: 'Charlotte Hornets',
+            colors: ['turquoise', 'purple'],
+            players: {
+                'Jeff Adrien': {
+                    number: 4,
+                    shoe: 18,
+                    points: 10,
+                    rebounds: 1,
+                    assists: 1,
+                    steals: 2,
+                    blocks: 7,
+                    slamDunks: 2
                 },
-                "Bismak Biyombo": {
-                    "Number":0,
-                    "shoe":16,
-                    "points":12,
-                    "rebounds":4,
-                    "assists":7,
-                    "steals":7,
-                    "blocks":15,
-                    "slamDunks":10,                },
-                
-                    "DeSagna Diop": {
-                    "number":2,
-                    "shoe":14,
-                    "points":24,
-                    "rebounds":12,
-                    "assists":12,
-                    "steals":4,
-                    "blocks":5,
-                    "slamDunks":5,
+
+                'Bismak Biyombo': {
+                    number: 0,
+                    shoe: 16,
+                    points: 12,
+                    rebounds: 4,
+                    assists: 7,
+                    steals: 7,
+                    blocks: 15,
+                    slamDunks: 10
                 },
-                "Ben Gordon": {
-                    "number":8,
-                    "shoe":15,
-                    "points":33,
-                    "rebounds":3,
-                    "assists":2,
-                    "steals":1,
-                    "blocks":1,
-                    "slamDunks":0,
+                'DeSagna Diop': {
+                    number: 4,
+                    shoe: 14,
+                    points: 24,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 4,
+                    blocks: 5,
+                    slamDunks: 5
                 },
-                "Brendan Haywood": {
-                    "number":33,
-                    "shoe":15,
-                    "points":6,
-                    "rebounds":12,
-                    "assists":12,
-                    "steals":22,
-                    "blocks":5,
-                    "slamDunks":12,
+                'Ben Gordon': {
+                    number: 8,
+                    shoe: 15,
+                    points: 33,
+                    rebounds: 3,
+                    assists: 2,
+                    steals: 1,
+                    blocks: 1,
+                    slamDunks: 0
                 },
-            },
-        },
+                'Brendan Haywood': {
+                    number: 33,
+                    shoe: 15,
+                    points: 6,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 22,
+                    blocks: 5,
+                    slamDunks: 12
+                }
+
+            }
+
+        }
+
     }
 }
-const game = gameObject()
-const homeTeam = game.home
-const awayTeam = game.away
-const players = [game.home.players,game.away.players]
-function allPlayers(){
-    const all = players.map(function(team){
-        const newArr = []
-        for(const player in team){
-            newArr.push({nameOfPlayer: player, ...team[player]})
+
+
+const { home: { players: homePlayers }, away: { players: awayPlayers } } = gameObject()
+
+const { home: { teamName: homeTeamName, colors: homeColors }, away: { teamName: awayTeamName, colors: awayColors } } = gameObject()
+
+const allPlayers = () => {
+    const totalPlayers = Object.assign(homePlayers, awayPlayers)
+    return totalPlayers
+}
+
+const homePlayersPoints = Object.values(homePlayers).map(function (hp) {
+    return [hp.points]
+}).flat()
+
+const awayPlayersPoints = Object.values(awayPlayers).map(function (ap) {
+    return [ap.points]
+}).flat()
+
+
+function totalPointsHome() {
+    return homePlayersPoints.reduce(function (previousValue, currentValue) {
+        return previousValue + currentValue
+    }, 0)
+}
+
+function totalPointsAway() {
+    return awayPlayersPoints.reduce(function (previousValue, currentValue) {
+        return previousValue + currentValue
+    }, 0)
+}
+
+
+
+function numPointsScored(playersName) {
+
+    for (const player in allPlayers()) {
+        if (player === playersName) {
+
+            return allPlayers()[player].points
         }
-        return newArr
-    })
-    return all.flat()
+    }
 }
 
-function findPlayer(playerName) {
-    return allPlayers().find(player => {
-        return player.nameOfPlayer === playerName
-    }).points
+function shoeSize(playersName) {
+
+    for (const player in allPlayers()) {
+        if (player === playersName) {
+
+            return allPlayers()[player].shoe
+        }
+    }
 }
 
-function numPointsScored(playersName){
-   return findPlayer(playersName).points
-    
-}   
 
-function shoeSize() {
-    return findPlayer(playersName).shoe
-}
-function isHome(team) {
-    return teamName === homeTeam.teamName
-}
+
+
 function teamColors(teamName) {
-if(teamName === homeTeam.teamName) {
-    return homeTeam.colors
+    if (awayTeamName === teamName) {
+        return awayColors
     }
-    return awayTeam.colors
+    else (homeTeamName === teamName)
+    return homeColors
 }
 
-function doesLongNameStealATon() {
-    const longestName = {length:0, player: ''}
-    const moststeals = {steals: 0, player: ''}
-    for(const player of allPlayers()){
-        if(player.nameOfPlayer.length > longestName.length ){
-            longestName.length = player.nameOfPlayer.length
-           longestName.player = player.nameOfPlayer
-        }
-        if(player.steals > moststeals.length) {
-            moststeals.steals = player.steals
-            moststeals.player = player.nameOfPlayer
-        }
-        
+
+const teamNames = () => {
+    const teamArr = Object.values(gameObject()).map(function (go) {
+        return [go.teamName][0]
+    });
+    return teamArr
+
+}
+
+
+
+function playerNumbers(teamName) {
+    if (teamName === homeTeamName) {
+        const homeNumbers = Object.values(homePlayers).map(function (hp) {
+            return [hp.number]
+        }).flat()
+        return homeNumbers
     }
-    console.log({player: longestName.player}, {moststeals: moststeals.player})
-    return longestName.player === moststeals.player
-    //const results = allPlayers().reduce(function(a,b){
-        //return Math.max(a.nameOfPlayer.length, b.nameOfPlayer.length)
-    //})
-    
+    else (teamName === awayTeamName)
+    const awayNumbers = Object.values(awayPlayers).map(function (ap) {
+        return [ap.number]
+    }).flat()
+    return awayNumbers
+}
+
+
+function playerStats(playerName) {
+    for (const player in allPlayers()) {
+        if (player === playerName) {
+            return allPlayers()[player]
+        }
+    }
+
+}
+
+
+
+function bigShoeRebounds() {
+    const allShoeArr = Object.values(allPlayers()).map(function (ap) { return [ap.shoe] }).flat()
+    for (const player in allPlayers()) {
+        if (allPlayers()[player].shoe === Math.max(...allShoeArr)) {
+            return allPlayers()[player].rebounds
+        }
+    }
+}
+
+function mostPointsScored() {
+    const pointsArr = Object.values(allPlayers()).map(function (ap) { return [ap.points] }).flat()
+    for (const player in allPlayers()) {
+        if (allPlayers()[player].points === Math.max(...pointsArr)) {
+            return player
+        }
+    }
+}
+
+function winningTeam() {
+    if (totalPointsHome() > totalPointsAway()) {
+        return homeTeamName
+    }
+    else return awayTeamName
+
+}
+
+
+function playerWithLongestName(){
+   teamPlayerNameArr = Object.keys(allPlayers()) 
+   return teamPlayerNameArr.reduce((a, b) => a.length < b.length ? b : a,"");
+}
+
+console.log(playerWithLongestName())
+
+function doesLongNameStealATon(){
+
+    const allStealsArr = Object.values(allPlayers()).map(function (ap) { return [ap.steals] }).flat()
+    for (let player in allPlayers()) {
+        if (allPlayers()[player].steals === Math.max(...allStealsArr)) {
+        return player === playerWithLongestName()
+        }
+    }
 }
         
     
